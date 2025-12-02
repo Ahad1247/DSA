5656cdfa93a376967d2c390b25223d522ea1767f
Ahad RT <Ahad1247>
2025-12-02 19:57:37 +0600

    merge sort

---
+++ mergeSort.py
@@ -0,0 +1,42 @@
+def merge(arr,low,mid,high):
+    left = low
+    right = mid+1
+    temp = []
+
+    while(left <= mid and right <= high):
+        if arr[left]<= arr[right]:
+            temp.append(arr[left])
+            left+= 1
+        else:
+            temp.append(arr[right])
+            right+= 1
+
+    while(left <= mid):
+        temp.append(arr[left])
+        left+= 1
+
+    while(right <= high):
+        temp.append(arr[right])
+        right+= 1
+
+    i = low
+    while (i <= high):
+        arr[i] = temp[i-low]
+        i+= 1
+
+def mergeSort(arr,low,high):
+    #base case
+    if (low==high):
+        return
+    
+    mid = int((low+high)/2)
+    #left side
+    mergeSort(arr,low,mid)
+    #right side
+    mergeSort(arr,mid+1,high)
+    merge(arr, low, mid, high)
+
+array = [1,3,5,2,4,8,1,4,6,7,9,3,2,1,5,1,5,8,10,120,120,131,1]
+print("before:",array)
+mergeSort(array,0,len(array)-1)
+print("after:",array)
