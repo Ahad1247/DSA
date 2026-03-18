16807d31a2832cc453d23fc2a655fa0f509f557c
Ahad RT <Ahad1247>
2026-03-18 22:54:14 +0600

    graph representation

---
+++ graph/graph_list.py
@@ -0,0 +1,10 @@
+n , m = map(int,input().split())
+graph = {i:[] for i in range(1,n+1)}
+
+for _ in range(m):
+    u , v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for vertex in graph:
+    print(vertex, ":", graph[vertex])
